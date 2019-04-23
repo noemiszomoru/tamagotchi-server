a134846f5d9472aa37eb91195d3dbd5f26c83130
Noemi Szomoru <noemi.szomoru@gmail.com>
2019-04-23 23:33:06 +0300

    Clean code

--- controllers/LoginController.ts
+++ controllers/LoginController.ts
@@ -36,35 +36,27 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
     });
 
     app.post("/login", (req: any, res: any) => {
-        console.log('0');
 
         db.query('SELECT * FROM `users` WHERE email=?', [req.body.email], (err: any, rows: User[]) => {
-            console.log('1');
             if (err) {
                 return res.status(500).json(err);
             }
-            console.log('2');
 
             if (rows.length != 1) {
                 return res.status(404).send('User not found!');
             }
-            console.log('3');
-            // am un row si returnez lucruri util loginului
+
             const user = rows[0];
             const result = bcrypt.compareSync(req.body.password, (user as any).password);
-            console.log('4');
             if (!result) {
                 return res.status(401).send('Password not valid!');
             }
-            console.log('5');
             const expiresIn = 24 * 60 * 60;
             const accessToken = jwt.sign({ id: user.pk }, SECRET_KEY, {
                 expiresIn: expiresIn
             });
-            console.log('6');
 
             res.json({ "user": User.create(user), "access_token": accessToken, "expires_in": expiresIn });
-            console.log('7');
         });
     });
 }
