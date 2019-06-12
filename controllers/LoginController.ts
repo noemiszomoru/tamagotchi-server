0eeec1f8fec9b36aa096e0d04b23f60784b2c61c
Florentin Szomoru <florentin.szomoru@nc-vision.com>
2019-06-13 01:33:06 +0300

    Update email template

--- controllers/LoginController.ts
+++ controllers/LoginController.ts
@@ -160,7 +160,7 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
 <table align="center" cellpadding="0" cellspacing="0" class="page-center" style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
 <tbody><tr>
 <td style="padding-top: 24px;">
-<img src="http://localhost:4200/assets/logo.png" style="width: 56px;">
+<img src="https://tamagotchi.freejack.ro/assets/logo.png" style="width: 56px;">
 </td>
 </tr>
 <tr>
@@ -187,42 +187,9 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
 </tr>
 <tr>
 <td>
-<a data-click-track-id="37" href="http://localhost:4200/token/${accessToken}" style="margin-top: 36px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #00cc99; border-radius: 28px; display: block; text-align: center; text-transform: uppercase" target="_blank">
+<a data-click-track-id="37" href="https://tamagotchi.freejack.ro/token/${accessToken}" style="margin-top: 36px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #00cc99; border-radius: 28px; display: block; text-align: center; text-transform: uppercase" target="_blank">
                                         Set Password
                                       </a>
-</td>
-</tr>
-</tbody></table>
-</td>
-</tr>
-</tbody></table>
-<table align="center" cellpadding="0" cellspacing="0" id="footer" style="background-color: #000; width: 100%; max-width: 680px; height: 100%;">
-<tbody><tr>
-<td>
-<table align="center" cellpadding="0" cellspacing="0" class="footer-center" style="text-align: left; width: 100%; padding-left: 120px; padding-right: 120px;">
-<tbody><tr>
-<td colspan="2" style="padding-top: 72px; padding-bottom: 24px; width: 100%;">
-<p style="width: 124px; height: 20px">Tamagotchi</p>
-</td>
-</tr>
-<tr>
-<td colspan="2" style="padding-top: 24px; padding-bottom: 48px;">
-<table cellpadding="0" cellspacing="0" style="width: 100%">
-<tbody><tr>
-<td style="width: 100%; height: 1px; max-height: 1px; background-color: #EAECF2; opacity: 0.19"></td>
-</tr>
-</tbody></table>
-</td>
-</tr>
-<tr>
-<td style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095A2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 15px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: 0; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
-                                          If you have any questions or concerns, we're here to help.
-                                        </td>
-</tr>
-<tr>
-<td style="height: 72px;"></td>
-</tr>
-</tbody></table>
 </td>
 </tr>
 </tbody></table>
