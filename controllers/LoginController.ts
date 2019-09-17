b7ea7d2b192fdc238395b25699a1346ba5a56cde
Noemi Szomoru <noemi.szomoru@gmail.com>
2019-09-18 00:35:16 +0300

    Email styling

--- controllers/LoginController.ts
+++ controllers/LoginController.ts
@@ -95,8 +95,8 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
                     port: 465,
                     secure: true,  //true for 465 port, false for other ports
                     auth: {
-                        user: 'noemi.szomoru@gmail.com',
-                        pass: 'Mahacskin13',
+                        user: 'noreply.tamagotchi@gmail.com',
+                        pass: 'Amadeo123!',
                         tls: {
                             rejectUnauthorized: false
                         }
@@ -159,12 +159,12 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
 <td>
 <table align="center" cellpadding="0" cellspacing="0" class="page-center" style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
 <tbody><tr>
-<td style="padding-top: 24px;">
-<img src="https://tamagotchi.freejack.ro/assets/logo.png" style="width: 56px; margin: 0 auto">
+<td style="padding-top: 24px; text-align: center;">
+<img src="https://tamagotchi.freejack.ro/assets/logo.png" style="width: 56px;">
 </td>
 </tr>
 <tr>
-<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 38px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">Set your password</td>
+<td colspan="2" style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 38px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none; text-align: center;">Set your password</td>
 </tr>
 <tr>
 <td style="padding-top: 48px; padding-bottom: 48px;">
@@ -187,7 +187,7 @@ export function LoginController(app: express.Express, db: mysql.Connection) {
 </tr>
 <tr>
 <td>
-<a data-click-track-id="37" href="https://tamagotchi.freejack.ro/token/${accessToken}" style="margin-top: 36px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #00cc99; border-radius: 28px; display: block; text-align: center; text-transform: uppercase" target="_blank">
+<a data-click-track-id="37" href="https://tamagotchi.freejack.ro/token/${accessToken}" style="margin: 36px auto; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #00cc99; border-radius: 28px; display: block; text-align: center; text-transform: uppercase" target="_blank">
                                         Set Password
                                       </a>
 </td>
